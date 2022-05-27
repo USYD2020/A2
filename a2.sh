e9ac2b22d70fd8db680a0d50a361da553ef21f21
Jeff <jeff2020@qq.com>
2022-05-27 16:28:46 +0800

    update

--- a2.sh
+++ a2.sh
@@ -1,6 +1,9 @@
 spark-submit \
-    --master yarn \
-    --deploy-mode cluster \
-    --num-executors 4 \
-    Comp5349-A2.py \
-    --output $1
+      --master yarn \
+      --deploy-mode client \
+      --num-executors 7 \
+       CUAD_data_spark_processing.py 
+
+	
+
+
